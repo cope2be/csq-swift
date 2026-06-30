2d4974881907f27a4a7c05d8403de2d6116f30fd
Cope <225660374+cope2be@users.noreply.github.com>
2026-06-30 20:19:25 +0700

    add stuff

---
+++ README.md
@@ -0,0 +1,19 @@
+# wwdc c dialect thing idk
+
+transpile to c99
+check with tinycc
+
+pointer types (nullable ~, non nullable *, owned ^)
+array slices
+
+kill goto
+kill lossy implicit conversion
+kill pointer arithmetic, type punning
+
+downgrade preprocessor (make #define like c#)
+fix switch (auto add break)
+
+lightweight generic(?)
+better mem management
+
+idk more
---
+++ csq/streams/str_stream.swift
@@ -0,0 +1,96 @@
+//
+//  str_stream.swift
+//  csq
+//
+//  Created by Cope on 30/6/2569 BE.
+//
+
+class str_stream
+{
+	private var remainder: Substring
+	
+	private var col: Int = 1
+	private var line: Int = 1
+	
+	init(_ src: String)
+	{
+		remainder = Substring(src)
+	}
+	
+	init(_ src: Substring)
+	{
+		remainder = src
+	}
+	
+	func get_col() -> Int
+	{
+		return col
+	}
+	
+	func get_line() -> Int
+	{
+		return line
+	}
+
+	func is_empty() -> Bool
+	{
+		return remainder.isEmpty
+	}
+		
+	@discardableResult
+	func consume() -> Bool
+	{
+		guard let c = remainder.first else { return false }
+		
+		if c.isNewline
+		{
+			col = 1
+			line += 1
+		}
+		else
+		{
+			col += 1
+		}
+		
+		remainder = remainder.dropFirst()
+		return true
+	}
+	
+	@discardableResult
+	func consume(_ dist: Int) -> Bool
+	{
+		for _ in 0..<max(dist, 0)
+		{
+			if !consume() { return false }
+		}
+		
+		return true
+	}
+	
+	func next() -> Character?
+	{
+		let c = peek()
+		if !consume() { return nil }
+		
+		return c
+	}
+	
+	func peek() -> Character?
+	{
+		return remainder.first
+	}
+	
+	// strs are slow in swift :v
+	func peek(_ dist: Int) -> Character?
+	{
+		guard
+			let idx = remainder.index(remainder.startIndex, offsetBy: dist, limitedBy: remainder.endIndex),
+			idx < remainder.endIndex
+		else
+		{
+			return nil
+		}
+		
+		return remainder[idx]
+	}
+}
